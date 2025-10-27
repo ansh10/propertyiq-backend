175282cdd0255535bf3bccbefcbb9014f8eead5b
ansh10 <anshkoladia@quotograph.io>
2025-10-27 01:41:02 -0600

    puhs

--- app.py
+++ app.py
@@ -108,4 +108,5 @@ def ping():
 
 # ---------- App Runner ----------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
